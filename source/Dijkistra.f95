abcd6587960cd6353b8415830da52275cc0ec035
CharlesGarrocho <charles.garrocho@gmail.com>
2013-05-14 08:05:25 -0300

    corrigido: tamanho da sintaxe do código.

--- source/Dijkistra.f95
+++ source/Dijkistra.f95
@@ -1,9 +1,9 @@
 program Dijkistra
     
     INTEGER, DIMENSION(:,:), ALLOCATABLE:: matriz
-    INTEGER, PARAMETER :: INFINITO = 1000000, MAXIMO = 4096
+    INTEGER, PARAMETER :: INF=2**30, MAXIMO = 4096
     CHARACTER*2048 :: linha
-    INTEGER:: l, coluna, erro, fim_arquivo, pos1, pos2, n, aux, origem, destino, minimo, indice, tam, tamanho, pos = 1
+    INTEGER:: l, coluna, erro, fim, pos1, pos2, n, aux, origem, destino, minimo, indice, tam, tamanho, pos = 1
     CHARACTER (LEN=32) :: arg, arq
     
     TYPE no
@@ -26,83 +26,76 @@ program Dijkistra
         STOP
     END IF
 
-    open(unit=12, file=arq, iostat=erro)
+    OPEN(UNIT=12, FILE=arq, IOSTAT=erro)
     
-    ! Verifica se o arquivo foi aberto comsucesso
     IF (erro == 0) THEN
 
-        ! Aloca um espaço na memória para uma matriz dinâmica(ponteiro para uma matriz de valores inteiros)
         ALLOCATE(matriz(MAXIMO,MAXIMO))
         
         l = 1
         coluna = 1
-        ! faz a leituta do arquivo para obter os valores que preencherão a matriz
         
-        do !1
-            read(12,'(A)', iostat=fim_arquivo) linha
-            if (fim_arquivo < 0) then
-                exit
-            end if
+        DO
+            READ(12,'(A)', IOSTAT=fim) linha
+            IF (fim < 0) THEN
+                EXIT
+            END IF
             
-            ! tokeniza a linha lida do arquivo para obter os valores das distâncias entre os elementos do grafo
             pos1 = 1
             n = 1
             
-            do !2
+            DO
                 pos2 = index(linha(pos1:), ' ')
-                if (linha(pos1+1:) == '' .and. linha(pos1+2:) == '') exit
-                if (pos2 == 0) then
-                    read(linha(pos1:), '(i10)') matriz(l,n)
-                    exit
-                end if
+                IF (linha(pos1+1:) == '' .AND. linha(pos1+2:) == '') EXIT
+                IF (pos2 == 0) THEN
+                    READ(linha(pos1:), '(i10)') matriz(l,n)
+                    EXIT
+                END IF
                 
-                ! armazena o valor extraido da string para a matriz de inteiros.
-                read(linha(pos1:pos1+pos2-2), '(i10)') matriz(l,n)
+                READ(linha(pos1:pos1+pos2-2), '(i10)') matriz(l,n)
                 n = n + 1
                 pos1 = pos2+pos1
-             end do !2
+             END DO
 
              l = l + 1
-        end do !1
+        END DO
         
         tamanho = n
 
-    else
-        print *,'Erro na abertura do arquivo!'
-        stop
-    end if
+    ELSE
+        PRINT *,'Erro ao Abrir o arquivo ', arq
+        STOP
+    END IF
     
-    ! inicialização do vetor de caminhos mais curtos com valores default
-    allocate(caminho(MAXIMO))
+    CLOSE(12)
+    
+    ALLOCATE(caminho(MAXIMO))
 
     DO indice = 1, MAXIMO 
         caminho(indice)%anterior = -1
-        caminho(indice)%distancia = INFINITO
+        caminho(indice)%distancia = INF
         caminho(indice)%proc = '0'
     END DO
     
     caminho(origem)%distancia = 0
     caminho(origem)%proc = '1'
     
-    ! --> Cálculo do caminho mais curto
     aux = origem
     l = 1
     coluna = 1
     pos1 = 1
     pos2 = 1
-    
-    !procura um caminho melhor para chegar até o nodo que está representado na variável "aux"
-    DO !1
-    
+ 
+    DO
     	IF (aux == destino) EXIT
-    	
+
     	indice = 1
-    	DO !2
+    	DO
     	
 			IF (indice > tamanho) EXIT
 			
 			!verifica se o nodo atual não é o nodo de origem ou se o nodo atual ainda não foi proc
-			IF ((matriz(aux,indice) /= INFINITO).AND.(matriz(aux,indice) /= 0).AND.(caminho(indice)%proc == '0')) THEN
+			IF ((matriz(aux,indice) /= INF).AND.(matriz(aux,indice) /= 0).AND.(caminho(indice)%proc == '0')) THEN
 				!verifica se a distância do nodo até a origem é maior que a distância de nodo vizinho + a distância
 				!entre o nodo atual até o nodo anterior
 				IF (caminho(aux)%distancia + matriz(aux,indice) < caminho(indice)%distancia) THEN
@@ -112,12 +105,12 @@ program Dijkistra
 			END IF
 			indice = indice + 1
 			
-    	END DO !2
+    	END DO
     	
     	aux = 1
-    	minimo = INFINITO
+    	minimo = INF
     	indice = 1
-    	DO !3
+    	DO
     		
     		IF (indice > tamanho) EXIT
     		!procura o nodo com menor distância para marcá-lo como proc
@@ -126,12 +119,12 @@ program Dijkistra
     			aux = indice
     		END IF
     		indice = indice + 1
-    	END DO !3
+    	END DO
     	caminho(aux)%proc = '1'
 
-    END DO !1
+    END DO
 
-    PRINT *, 'Caminho mais curto do nodo de origem até o nodo de destino: '
+    PRINT *, 'Caminho mais curto: '
 
     l = destino
     PRINT *, destino
@@ -142,7 +135,6 @@ program Dijkistra
     END DO
     PRINT *, 'Distância = ', caminho(destino)%distancia
 
-    CLOSE(12)
     DEALLOCATE(matriz)
     DEALLOCATE(caminho)
 END
